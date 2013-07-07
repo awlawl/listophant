a33775c2cba461a6b9a4cfe3eb3fe444dd2c1b1c
Allen Lyons <allen.lyons@gmail.com>
2013-07-07 21:33:03 +0000

    new text box

--- listophant.js
+++ listophant.js
@@ -1,10 +1,23 @@
+Lists = new Meteor.Collection("lists");
 Tasks = new Meteor.Collection("tasks");
 
 if (Meteor.isClient) {
 	Template.tasktemplate.tasks = function() {
-		var tasks = Tasks.find({});
-		return tasks;
+		var list = Lists.find({}).fetch()[0];
+		if (list) {
+			
+			return Tasks.find({listid:list._id});
+			
+		}
 	};
+	Template.tasktemplate.newtask = function() {
+		var list = Lists.find({});
+		console.dir(list);
+		//console.log('count = ' + list.count());
+
+		list.newtask = "new task";
+		return list.newtask;
+	}
 
 	Template.tasktemplate.done_checkbox = function() {
 		return this.isDone ? 'checked="checked"':''
@@ -12,9 +25,16 @@ if (Meteor.isClient) {
 
 	Template.tasktemplate.events({
 		'click .donecheckbox':function() {
+			console.dir(this._id);
 			Tasks.update(this._id,{$set: {isDone:!this.isDone}});
 			console.log("updating ");
+		},
+		'click .addtask': function() {
+			var list = Lists.find({}).fetch()[0];
+		
+			console.log('new task ' + Template.tasktemplate.find(".addtask").value());
 		}
+
 	});
 
  
@@ -22,17 +42,24 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 	Meteor.startup(function () {
 		console.log("remove all");
-		Tasks.remove({});
-      		var names = ["Ada Lovelace",
-                   "Grace Hopper",
-                   "Marie Curie",
-                   "Carl Friedrich Gauss",
-                   "Nikola Tesla",
-                   "Claude Shannon"];
-      		for (var i = 0; i < names.length; i++) {
-			console.log("adding " + names[i]);
-        		Tasks.insert({name: names[i], isDone: (i%2)==0});
-    		}
+		Lists.remove({});
+      		var taskNames = [
+			"Milk",
+			"Bread",
+			"Chicken",
+			"Fruit",
+			"Radishes"
+
+		];
+		
+		var list = {newtask:"new here"};
+		Lists.insert(list, function(err, id){ 
+			for(var i=0; i<taskNames.length; i++) {
+        			Tasks.insert({listid:id, name: taskNames[i], isDone: (i%2)==0});
+    			}
+		});
+
+
   });
 		
 }
