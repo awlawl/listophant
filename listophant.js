edd753a4d7290fccd89326fbd0e1dd314ee704fb
Allen Lyons <allen.lyons@gmail.com>
2013-07-08 01:51:10 +0000

    now adding tasks

--- listophant.js
+++ listophant.js
@@ -1,41 +1,77 @@
 Lists = new Meteor.Collection("lists");
 Tasks = new Meteor.Collection("tasks");
 
+// Creates an event handler for interpreting "escape", "return", and "blur"
+  // on a text field and calling "ok" or "cancel" callbacks.
+  var make_okcancel_handler = function (options) {
+    var ok = options.ok || function () {};
+    var cancel = options.cancel || function () {};
+    return function (evt) {
+      if (evt.type === "keydown" && evt.which === 27) {
+        // escape = cancel
+        cancel.call(this, evt);
+      } else if (evt.type === "keyup" && evt.which === 13) {
+        // blur/return/enter = ok/submit if non-empty
+        var value = String(evt.target.value || "");
+        if (value)
+          ok.call(this, value, evt);
+        else
+          cancel.call(this, evt);
+      }
+    };
+  };
+
 if (Meteor.isClient) {
+	var listid = null;
+
 	Template.tasktemplate.tasks = function() {
 		var list = Lists.find({}).fetch()[0];
 		if (list) {
-			
-			return Tasks.find({listid:list._id});
+			listid = list._id;
+			return Tasks.find({listid:listid});
 			
 		}
 	};
-	Template.tasktemplate.newtask = function() {
-		var list = Lists.find({});
-		console.dir(list);
-		//console.log('count = ' + list.count());
-
-		list.newtask = "new task";
-		return list.newtask;
-	}
-
+	
 	Template.tasktemplate.done_checkbox = function() {
 		return this.isDone ? 'checked="checked"':''
 	};
 
+	
 	Template.tasktemplate.events({
 		'click .donecheckbox':function() {
 			console.dir(this._id);
 			Tasks.update(this._id,{$set: {isDone:!this.isDone}});
 			console.log("updating ");
 		},
-		'click .addtask': function() {
-			var list = Lists.find({}).fetch()[0];
 		
-			console.log('new task ' + Template.tasktemplate.find(".addtask").value());
-		}
+		'keyup #newtask, keydown #newtask, focusout #newtask': make_okcancel_handler({
+			ok: function(text,event) {
+				saveTask(listid, text);
+				event.target.value="";
+			},
+			cancel: function(text, event) {
+				console.log("canceled");
+			}
+		})
+		
 
 	});
+	// Returns an event_map key for attaching "ok/cancel" events to
+  // a text input (given by selector)
+  var okcancel_events = function (selector) {
+    return 'keyup '+selector+', keydown '+selector+', focusout '+selector;
+  };
+  	var eventxx = okcancel_events("#newtask");
+
+  console.log(eventxx);
+  
+  
+  Template.tasktemplate.events[okcancel_events["#newtask"]] = make_okcancel_handler({
+	ok: function(text,event) {
+		console.log("you typed " + text );
+	}
+  });
 
  
 }
@@ -55,11 +91,15 @@ if (Meteor.isServer) {
 		var list = {newtask:"new here"};
 		Lists.insert(list, function(err, id){ 
 			for(var i=0; i<taskNames.length; i++) {
-        			Tasks.insert({listid:id, name: taskNames[i], isDone: (i%2)==0});
+        			saveTask(id,taskNames[i]);
     			}
 		});
 
 
   });
 		
+}
+
+function saveTask(id, taskName) {
+	Tasks.insert({listid:id, name: taskName, isDone: false});
 }
